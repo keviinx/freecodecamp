a62211b38e63c33f664e3a844fcb2af4255f44a2
Keviindran Ramachandran <keviinx@yahoo.com>
2021-03-25 09:20:45 +0100

    Added skeleton code.

--- python/boilerplate-budget-app/budget.py
+++ python/boilerplate-budget-app/budget.py
@@ -1,7 +1,39 @@
 class Category:
 
+    # accepts an amount and description.
+    # If no description is given, it should default to an empty spring.
+    # Should append an object to the ledger list in the form of {"amount": amount, "description": description}.
+    def deposit(amount, description = ""):
+        # TODO append based on the requirement
+        pass
 
+    # similar to deposit method, but the amount passed in should be stored in the ledger as a negative number.
+    # If there are not enough funds, nothing should be added to the ledger.
+    # This method should return True if the withdrawal took place, and False otherwise.
+    def withdraw(amount, description = ""):
+        # TODO append based on the requirement
+        pass
 
+    # method that returns the current balance of the budget category based on the deposits and withdrawals that have occurred.
+    def get_balance():
+        # TODO
+        pass
 
+    # method that accepts an amount and another budget category as arguments.
+    # The method should add a withdrawal with the amount and the description "Transfer to [Destination Budget Category]".
+    # The method should then add a deposit to the other budget category with the amount and the description "Transfer from [Source Budget Category]".
+    # If there are not enough funds, nothing should be added to either ledgers. This method should return True if the transfer took place, and False otherwise.
+    def transfer():
+        # TODO
+        pass
+
+    # method that accepts an amount as an argument.
+    # It returns False if the amount is greater than the balance of the budget category and returns True otherwise.
+    # This method should be used by both the withdraw method and transfer method.
+    def check_funds():
+        # TODO
+        pass
 
 def create_spend_chart(categories):
+    #TODO
+    pass
