282a4d8454d32ba44144065c255c31d485774d9f
Keviindran Ramachandran <keviinx@yahoo.com>
2021-04-19 09:51:29 +0200

    adapted the comments for better readability.

--- python/boilerplate-budget-app/budget.py
+++ python/boilerplate-budget-app/budget.py
@@ -9,17 +9,17 @@ class Category:
 
     # accepts an amount and description.
     # If no description is given, it should default to an empty spring.
-    # Should append an object to the ledger list in the form of {"amount": amount, "description": description}.
     def deposit(self, amount, description = ""):
+        # Should append an object to the ledger list in the form of {"amount": amount, "description": description}.
         self.ledger.append({"amount": amount, "description": description})
 
     # similar to deposit method, but the amount passed in should be stored in the ledger as a negative number.
-    # If there are not enough funds, nothing should be added to the ledger.
-    # This method should return True if the withdrawal took place, and False otherwise.
     def withdraw(self, amount, description = ""):
+        # If there are not enough funds, nothing should be added to the ledger.
         withdrawal_took_place = False
         if self.check_funds(amount):
             self.ledger.append({"amount": -amount, "description": description})
+            # This method should return True if the withdrawal took place, and False otherwise.
             withdrawal_took_place = True
         return withdrawal_took_place
 
@@ -29,25 +29,22 @@ class Category:
         return sum(i["amount"] for i in self.ledger)
 
     # method that accepts an amount and another budget category as arguments.
-    # The method should add a withdrawal with the amount and the description "Transfer to [Destination Budget Category]".
-    # The method should then add a deposit to the other budget category with the amount and the description "Transfer from [Source Budget Category]".
-    # If there are not enough funds, nothing should be added to either ledgers. This method should return True if the transfer took place, and False otherwise.
     def transfer(self, amount, budget_category):
         transfer_took_place = False
-        # If amount is enough
+        # If there are not enough funds, nothing should be added to either ledgers.
         if self.check_funds(amount):
-            # withdraw from self
+            # The method should add a withdrawal with the amount and the description "Transfer to [Destination Budget Category]".
             self.withdraw(amount, "Transfer to " + budget_category.category.capitalize())
-            # deposit into another budget category
+            # The method should then add a deposit to the other budget category with the amount and the description "Transfer from [Source Budget Category]".
             budget_category.deposit(amount, "Transfer from " + self.category.capitalize())
-            # set transfer took place to true
+            # This method should return True if the transfer took place, and False otherwise.
             transfer_took_place = True
         return transfer_took_place
 
     # method that accepts an amount as an argument.
-    # It returns False if the amount is greater than the balance of the budget category and returns True otherwise.
     # This method should be used by both the withdraw method and transfer method.
     def check_funds(self, amount):
+        # It returns False if the amount is greater than the balance of the budget category and returns True otherwise.
         amount_is_lesser = True
         if amount > self.get_balance():
             amount_is_lesser = False
@@ -68,7 +65,17 @@ class Category:
         output += f'Total: {self.get_balance()}'
         return output
 
+# Besides the `Category` class, create a function (outside of the class) called `create_spend_chart` that takes a list of categories as an argument. 
+# It should return a string that is a bar chart.
 
+# The chart should show the percentage spent in each category passed in to the function. 
+# The percentage spent should be calculated only with withdrawals and not with deposits. 
+# Down the left side of the chart should be labels 0 - 100. 
+# The "bars" in the bar chart should be made out of the "o" character. 
+# The height of each bar should be rounded down to the nearest 10. 
+# The horizontal line below the bars should go two spaces past the final bar. 
+# Each category name should be written vertically below the bar. 
+# There should be a title at the top that says "Percentage spent by category".
 def create_spend_chart(categories):
     #TODO
     pass
