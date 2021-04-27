3b545f2f9955d61772a9e46bbd59099735cde0c4
Keviindran Ramachandran <keviinx@yahoo.com>
2021-04-27 12:13:53 +0200

    added __init__, set_width, set_height, get_area and get_perimeter for Rectangle class.

--- python/boilerplate-polygon-area-calculator/shape_calculator.py
+++ python/boilerplate-polygon-area-calculator/shape_calculator.py
@@ -1,6 +1,25 @@
 class Rectangle:
 
+    # When a Rectangle object is created, it should be initialized with width and height attributes.
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
 
+    # set the width based on the passed argument
+    def set_width(self, width):
+        self.width = width
 
+    # set the height based on the passed argument
+    def set_height(self, height):
+        self.height = height
 
-class Square:
+    # Returns area (width * height)
+    def get_area(self):
+        return (self.width * self.height)
+
+    # Returns perimeter (2 * width + 2 * height)
+    def get_perimeter(self):
+        return (2 * self.width + 2* self.height)
+
+class Square(Rectangle):
+    pass
